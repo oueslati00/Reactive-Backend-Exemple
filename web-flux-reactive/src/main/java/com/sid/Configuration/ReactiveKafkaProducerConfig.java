61bcd2c919d786744b38c81d7de0f155c09497f8
BRAHIM Oueslati <boueslati76@gmail.com>
2021-10-01 20:30:15 +0100

    setup simple kafka producer : bug

---
+++ web-flux-reactive/src/main/java/com/sid/Configuration/ReactiveKafkaProducerConfig.java
@@ -0,0 +1,19 @@
+package com.sid.Configuration;
+
+import com.sid.DTO.ProducerDTO;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import reactor.kafka.sender.SenderOptions;
+
+import java.util.Map;
+
+@Configuration
+public class ReactiveKafkaProducerConfig {
+    @Bean
+    public ReactiveKafkaProducerTemplate<String, ProducerDTO> reactiveKafkaProducerTemplate(KafkaProperties properties){
+        Map<String, Object> props = properties.buildProducerProperties();
+        return new ReactiveKafkaProducerTemplate<>(SenderOptions.create(props));
+    }
+}
---
+++ web-flux-reactive/src/main/java/com/sid/Service/ReactiveProducerService.java
@@ -0,0 +1,32 @@
+package com.sid.Service;
+
+
+import com.sid.DTO.ProducerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReactiveProducerService {
+    private final Logger log = LoggerFactory.getLogger(ReactiveProducerService.class);
+    private final ReactiveKafkaProducerTemplate<String, ProducerDTO> reactiveKafkaProducerTemplate;
+
+    @Value(value = "${PRODUCER_DTO_TOPIC}")
+    private String topic;
+
+    public ReactiveProducerService(ReactiveKafkaProducerTemplate<String, ProducerDTO> reactiveKafkaProducerTemplate) {
+        this.reactiveKafkaProducerTemplate = reactiveKafkaProducerTemplate;
+    }
+    public void send(ProducerDTO producerDTO) {
+        log.info("send to topic={}, {}={},", topic, ProducerDTO.class.getSimpleName(), producerDTO);
+        reactiveKafkaProducerTemplate.send(topic, producerDTO)
+                .doOnSuccess(senderResult -> log.info("sent {} offset : {}", producerDTO, senderResult.recordMetadata().offset()))
+                .subscribe(
+                        data ->{
+                            System.out.println(data);
+                        }
+                );
+    }
+}
